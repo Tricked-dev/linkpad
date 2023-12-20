d207a5e281675e506b4b4d23258a3c5b18bd1a1d
Tricked <tricked@tricked.pro>
2023-12-20 22:30:32 +0100

    feat: init

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,58 @@
+<script lang="ts">
+  import Dialog from "./Dialog.svelte";
+
+  const css = "rounded-24px bg-surface-200/30 backdrop-blur-4 p-4 w-160px h-160px text-3xl font-bold text-white hover:bg-surface-200/50 duration-200"
+
+  const edit = true
+  let open = $state(false);
+</script>
+
 <main
-  class="[font-family:'Lato'] [background-image:url('/image.png')] min-h-screen"
+  class="[font-family:'Lato'] [background-image:url('/bg3.png')] min-h-screen"
   data-theme="ato"
 >
-  <div class="bg-surface-200/50 backdrop-blur-50 p-4">
+  <div class="bg-surface-200/50 backdrop-blur-50 p-4 text-white flex">
     <span class="text text-3xl">LINK PAD</span>
+
+    {#if edit}<span class="ml-auto mt-auto">Edit mode</span>{/if}
   </div>
 
-  <div></div>
+  <div class="p-4 flex flex-col gap-4">
+    <div class="flex gap-4">
+    		{@render button("Button 1")}
+    		{@render button("Button 2")}
+    		{@render button("Button 3")}
+    		{@render button("Button 4")}
+    		{@render button("Button 5")}
+    </div>
+     <div class="flex gap-4">
+    		{@render button("Action 1")}
+    		{@render button("Cool BTN")}
+    		{@render button("Text")}
+    </div>
+       <div class="flex gap-4">
+    		{@render button("Action 1")}
+    		{@render button("Cool BTN")}
+    		{@render button("Cool BTN")}
+    		{@render button("Text")}
+        {#if edit}<button class={`${css} border-tertiary-500 border-4`}>
+            <div class="i-mdi-plus text-9xl">
+
+            </div>
+        </button>{/if}
+    </div>
+  </div>
+<Dialog bind:open={open} >
+    <div class="h-20 w-70">
+    Editiing!
+
+    </div>
+  </Dialog>
 </main>
 
 {#snippet button(text)}
-    <button class="">
-
+    <button class={css}>
+      {#if edit}<div onclick={() => open = true} class="i-mdi-gear text-2xl absolute top-2 right-2 rounded-lg hover:bg-primary-500"></div>{/if}
+        {text}
     </button>
 {/snippet}
---
+++ src/Dialog.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+
+  let { open, children, duration=150 } = $props<{ open: boolean, duration?: number, children:any}>();
+
+  function close() {
+    open = false;
+  }
+</script>
+
+{#if open}
+  <dialog onclose={close} {open} class="flex justify-center" transition:fade={{ duration }}>
+    <div class="absolute backdrop-blur-10 bg-surface-200/5 p-20 fixed inset-0 grid place-content-center" onclick={close}>
+        <div class="w-full max-w-lg bg-surface-500 p-4 shadow-lg text-white rounded-lg" onclick={(e) =>e.stopPropagation()}>
+            {@render children()}
+        </div>
+    </div>
+  </dialog>
+{/if}
