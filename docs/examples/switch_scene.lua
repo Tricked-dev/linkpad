018207c5fa0037e56552098abb061bf59cb6071f
Tricked <tricked@tricked.pro>
2024-03-02 14:41:08 +0100

    docs

--- docs/examples/switch_scene.lua
+++ docs/examples/switch_scene.lua
@@ -15,28 +15,4 @@ mod.on_click = function()
     end
 end
 
-mod.on_long_click = function()
-    print("long clicked")
-end
-
-mod.get_icon = function()
-    -- return {
-    --     path = "notify.png"
-    -- }
-    -- return {
-    --     data =
-    --     "base64 encoded svg png or elsewere"
-    --     ,
-    --     content_type = "image/svg"
-    -- }
-    return {
-        text = last
-    }
-    -- return {
-    --     type = "url",
-    --     url = "https://api.picup.click/cdn/55383857-37da-412b-bd61-3da90366ef35/01HM0ZS2PE7DWDK29VYRKC58RQ.png"
-    -- }
-end
-
-
 return mod
